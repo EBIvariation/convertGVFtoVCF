47684b7bfeb2357de6429207f6d5d27e282bc03c
Kirstie Hetherington <hetherin@ebi.ac.uk>
2025-09-09 15:41:30 +0100

    removed new lines

--- convert_gvf_to_vcf/convertGVFtoVCF.py
+++ convert_gvf_to_vcf/convertGVFtoVCF.py
@@ -591,11 +591,9 @@ def generate_vcf_metainformation(lines_custom_unstructured, gvf_pragmas, gvf_non
         else:
             pass
     print("Total number of samples in this VCF: ", len(sample_names))
-
     for pragma in pragmas_to_add:
         if pragma not in unique_pragmas_to_add:
             unique_pragmas_to_add.append(pragma)
-
     return unique_pragmas_to_add, sample_names
 
 # step 10
