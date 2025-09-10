08ffe608c0abe6b0ce6fe108366a3596dd7f4fbc
Kirstie Hetherington <hetherin@ebi.ac.uk>
2025-09-10 11:49:42 +0100

    added test for format_sample_values

--- tests/test_convert_gvf_to_vcf.py
+++ tests/test_convert_gvf_to_vcf.py
@@ -5,7 +5,8 @@ from convert_gvf_to_vcf.convertGVFtoVCF import generate_custom_unstructured_meta
     read_dgva_info_attributes, read_gvf_info_attributes, gvf_features_to_vcf_objects, format_vcf_datalines, \
     generate_vcf_metainformation, generate_all_possible_standard_structured_info_lines, \
     generate_all_possible_standard_structured_alt_lines, generate_all_possible_standard_structured_filter_lines, \
-    generate_all_possible_standard_structured_format_lines, generate_vcf_header_line, populate_sample_formats
+    generate_all_possible_standard_structured_format_lines, generate_vcf_header_line, populate_sample_formats, \
+    format_sample_values
 from convert_gvf_to_vcf.convertGVFtoVCF import VcfLine, GvfFeatureline
 
 
@@ -213,7 +214,42 @@ class TestConvertGVFtoVCF(unittest.TestCase):
 
 
     def test_format_sample_values(self):
-        pass
+        gvf_pragmas, gvf_non_essential, gvf_lines_obj_list = read_in_gvf_file(self.input_file)
+        dgva_attribute_dict = read_dgva_info_attributes(self.dgva_input_file)
+        gvf_attribute_dict = read_gvf_info_attributes(self.gvf_input_file)
+        # custom meta-information lines for this VCF file
+        lines_custom_structured = []
+        lines_custom_unstructured = []
+        # standard structured meta-information lines for this VCF file
+        lines_standard_ALT = []
+        lines_standard_INFO = []
+        lines_standard_FILTER = []
+        lines_standard_FORMAT = []
+        # Dictionary for all possible VCF meta-information lines
+        all_possible_INFO_lines = generate_all_possible_standard_structured_info_lines()
+        all_possible_ALT_lines = generate_all_possible_standard_structured_alt_lines()
+        all_possible_FILTER_lines = generate_all_possible_standard_structured_filter_lines()
+        all_possible_FORMAT_lines = generate_all_possible_standard_structured_format_lines()
+
+        vcf_data_lines, list_of_vcf_objects = gvf_features_to_vcf_objects(gvf_lines_obj_list,
+                                                                          dgva_attribute_dict,
+                                                                          gvf_attribute_dict,
+                                                                          lines_custom_structured,
+                                                                          lines_standard_ALT,
+                                                                          lines_standard_INFO,
+                                                                          lines_standard_FILTER,
+                                                                          lines_standard_FORMAT,
+                                                                          all_possible_ALT_lines,
+                                                                          all_possible_INFO_lines,
+                                                                          all_possible_FILTER_lines,
+                                                                          all_possible_FORMAT_lines
+                                                                          )
+        lines_custom_unstructured = ['##fileformat=VCFv4.4','##fileDate=20150715', '##source=DGVa','##source=DGVa', '##genome-build=NCBI GRCz10']
+        unique_pragmas_to_add, samples = generate_vcf_metainformation(lines_custom_unstructured, gvf_pragmas,
+                                                                      gvf_non_essential, list_of_vcf_objects)
+        sample_name_format_value = populate_sample_formats(samples)
+        sample_format_values_string = format_sample_values(sample_name_format_value)
+        assert isinstance(sample_format_values_string, str)
 
     def test_format_vcf_datalines(self):
         gvf_pragmas, gvf_non_essential, gvf_lines_obj_list = read_in_gvf_file(self.input_file)
