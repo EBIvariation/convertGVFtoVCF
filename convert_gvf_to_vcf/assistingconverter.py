576ebee8b8dab4725c848e891d6927c239fd975c
khetherin <hetherin@ebi.ac.uk>
2025-10-23 11:35:01 +0100

    populated info

--- convert_gvf_to_vcf/assistingconverter.py
+++ convert_gvf_to_vcf/assistingconverter.py
@@ -30,16 +30,27 @@ class Assistingconverter:
                                              info_attribute_dict,
                                              field_lines_dictionary,
                                              all_possible_lines_dictionary):
+        # this converts GVF attributes to a dictionary that will make VCF values
+        # this also populates ALT INFO FILTER FORMAT with the correct VCF values.
         gvf_attribute_dictionary = get_gvf_attributes(column9_of_gvf)
-        vcf_vals = {}
+        vcf_info_values = {} # key is info field value; value is value
+        info = []
         catching_for_review = []
-        # print("dgva_attribute_dict", dgva_attribute_dict)
         mapping_attribute_dict = read_info_attributes(os.path.join(etc_folder, 'attribute_mapper.tsv'))
+        print("dgva_attribute_dict", gvf_attribute_dictionary) #GVF line in dgva
+        print("info_attribute_dict", info_attribute_dict) # INFOattributes.tsv
+        print("field_lines_dictionary", field_lines_dictionary) # dictionaries for ALT INFO FILTER FORMAT
+        print("mapping_attribute_dict", mapping_attribute_dict) # attribute mapper
         # created a rough guide to attributes_for_custom_structured_metainformation in INFOattributes.tsv = this probably should be refined at a later date
         # TODO: edit INFOattributes.tsv i.e. replace unknown placeholders '.' with the actual answer, provide a more informative description
+        # for each attribute in the DGVa GVF file,
+        # if the attributes is in the INFOattributes.tsv add to INFO dictionary
+        # else if attirbute is in attribute_mapper.tsv add to its specific dictionary (INFO/FORMAT)
+        # else if attribute is a specific case, deal with appropriately.
         for attrib_key in gvf_attribute_dictionary:
             # if dgva specific key, create custom INFO tag's meta information line
             if attrib_key in info_attribute_dict:
+                # create and store header line
                 field_lines_dictionary["INFO"].append(
                     generate_custom_structured_meta_line(
                         vcf_key="INFO", vcf_key_id=attrib_key,
@@ -48,23 +59,14 @@ class Assistingconverter:
                         vcf_key_description=info_attribute_dict[attrib_key][3],
                         optional_extra_fields=None)
                 )
-                vcf_vals[attrib_key] = gvf_attribute_dictionary[attrib_key]
+                # store INFO values
+                vcf_info_values[attrib_key] = gvf_attribute_dictionary[attrib_key]
             elif attrib_key in mapping_attribute_dict:
-                field = mapping_attribute_dict[attrib_key][1]
-                key_for_field = mapping_attribute_dict[attrib_key][2]
-                field_lines_dictionary[field].append(all_possible_lines_dictionary[field][key_for_field])
-
-            elif attrib_key == "sample_name":
-                # sample_names.append(sample_names)
-                pass
+                # create and store header line
+                field_lines_dictionary[mapping_attribute_dict[attrib_key][1]].append(all_possible_lines_dictionary[mapping_attribute_dict[attrib_key][1]][mapping_attribute_dict[attrib_key][2]])
+                # store INFO values
+                vcf_info_values[mapping_attribute_dict[attrib_key][2]] = gvf_attribute_dictionary[attrib_key]
             # GVF keys (not dgva specific)
-            elif attrib_key == "ID":
-                pass
-            elif attrib_key == "Variant_seq":
-                pass
-            elif attrib_key == "Reference_seq":
-                pass
-
             elif attrib_key == "Dbxref":
                 # custom info tag + pase and add to id?
                 pass
@@ -77,30 +79,20 @@ class Assistingconverter:
             elif attrib_key == "Phased":
                 # GT or FORMAT PS
                 pass
-            elif attrib_key == "Start_range":
-                # either custom info tag or CIPOS or CIEND, may need imprecise
-                pass
-            elif attrib_key == "End_range":
-                # either custom info tag or CIEND, may need imprecise
-                pass
             elif attrib_key == "Breakpoint_range":
                 # either custom info tag or CIPOS, CIEND, may need imprecise
                 pass
             elif attrib_key == "Individual":
                 # sampl name for each column
                 pass
-            # elif attrib_key == "Total_reads":
-            #     # reserved info key, DP
-            #     pass
-            # elif attrib_key == "Variant_freq":
-            #     # reserve info tag, AF
-            #     pass
-            # elif attrib_key == "Genotype":
-            #     # GT
-            #     pass
             else:
                 print("catching these attribute keys for review at a later date", attrib_key)
                 catching_for_review.append(attrib_key)
-        # print("dictionary", gvf_attribute_dictionary)
-        # print("vcf_vals", vcf_vals)
+        print("gvf_attribute_dictionary", gvf_attribute_dictionary) # this will be self.vcf_value in VCFline
+        print(field_lines_dictionary["INFO"])
+        print(vcf_info_values)
+
+        info_string = ''.join(f'{key}={value};' for key, value in vcf_info_values.items()).rstrip(';')
+        # mapping attribute dictionary
+        return gvf_attribute_dictionary, info_string
         return gvf_attribute_dictionary
