40945ad51831fd9fc2ff38c1eb82238944e8aea5
khetherin <hetherin@ebi.ac.uk>
2025-10-23 14:41:18 +0100

    removed helpers.py, added generate_custom_structured_meta_line to assistingconverter.py and removed the class for Asssisting converter

--- convert_gvf_to_vcf/assistingconverter.py
+++ convert_gvf_to_vcf/assistingconverter.py
@@ -1,10 +1,35 @@
 # this is an assistant converter to help convert gvf attributes
 import os
 from convert_gvf_to_vcf.utils import read_info_attributes
-from convert_gvf_to_vcf.helpers import generate_custom_structured_meta_line
+
 # setting up paths to useful directories
 convert_gvf_to_vcf_folder = os.path.dirname(__file__)
 etc_folder = os.path.join(convert_gvf_to_vcf_folder, 'etc')
+
+def generate_custom_structured_meta_line(vcf_key, vcf_key_id, vcf_key_number, vcf_key_type, vcf_key_description,
+                                         optional_extra_fields=None):
+    """ Generates a custom structured meta-information line for INFO/FILTER/FORMAT/ALT
+    :param vcf_key: required field INFO, FILTER, FORMAT, ALT
+    :param vcf_key_id: required field for structured lines ID
+    :param vcf_key_number: The number of values that can be included or special character: A or R or G or .
+    :param vcf_key_type: Values are Integer, Float, Character, String
+    :param vcf_key_description: Description
+    :param optional_extra_fields: an optional field, dictionary of custom fields and their values
+    :return: custom_structured_string
+    """
+    extra_keys_kv_lines = []
+    if optional_extra_fields:
+        for extra_field in optional_extra_fields:
+            kv_line = "," + extra_field + "=" + '"' + optional_extra_fields[extra_field] + '"'
+            extra_keys_kv_lines.append(kv_line)
+    vcf_key_extra_keys = ''.join(extra_keys_kv_lines)
+    custom_structured_string = (f'##{vcf_key}=<'
+                                f'ID={vcf_key_id},'
+                                f'Number={vcf_key_number},'
+                                f'Type={vcf_key_type},'
+                                f'Description="{vcf_key_description}"'
+                                f'{vcf_key_extra_keys}>')
+    return custom_structured_string
 
 def get_gvf_attributes(column9_of_gvf):
     """Get a dictionary of GVF attributes
