bdc9c754ba7110eb004e9065220a77010157e512
khetherin <hetherin@ebi.ac.uk>
2025-11-03 16:20:09 +0000

    implemented the logger

---
+++ convert_gvf_to_vcf/logger.py
@@ -0,0 +1,14 @@
+"""This sets up the logger for logging messages"""
+import logging
+import os
+# Create logger for module name
+logger = logging.getLogger(__name__)
+
+# Set up paths
+input_folder = os.path.dirname(__file__)
+output_folder = os.path.join(input_folder, "..","tests", "output")
+log_path = os.path.join(output_folder, "converted.log")
+print("log path is ", log_path)
+logging.basicConfig(filename=log_path,
+                    level=logging.DEBUG,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
