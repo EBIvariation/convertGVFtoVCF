90743d188808b8bc7b0d25db91ccc7d2eaa69243
khetherin <hetherin@ebi.ac.uk>
2025-12-01 12:11:05 +0000

    changed test_generate_vcf_metainfo

--- tests/test_convert_gvf_to_vcf.py
+++ tests/test_convert_gvf_to_vcf.py
@@ -94,13 +94,18 @@ class TestConvertGVFtoVCF(unittest.TestCase):
         )
         print(unique_pragmas_to_add)
         assert unique_pragmas_to_add == ['##fileformat=VCFv4.4', '##gff-version=3', '##source=DGVa', '##gvf-version=1.06',
-                                         '##species=http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=7955', '##fileDate=2015-07-15',
-                                         '##genome-build=NCBIGRCz10', '##Study_accession=nstd62', '##Study_type=Control Set',
-                                         '##Display_name=Brown_et_al_2012', '##Publication_year=2012',
+                                         '##species=http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=7955',
+                                         '##fileDate=2015-07-15', '##genome-build=NCBIGRCz10', '##Study_accession=nstd62',
+                                         '##Study_type=Control Set', '##Display_name=Brown_et_al_2012', '##PMID=22203992',
+                                         '##Journal=Proceedings of the National Academy of Sciences of the United States of America',
+                                         '##Paper_title=Extensive genetic diversity and substructuring among zebrafish strains revealed through copy number variant analysis.',
+                                         '##Publication_year=2012', '##First_author=Kim Brown',
                                          '##Description=Comparative genomic hybridization analysis of 3 laboratory and one wild zebrafish populations for Copy Number Variants',
-                                         '##Assembly_name=GRCz10', '##subject=subject_name=Wilds2-3', '##subject=subject_name=Zon9', '##subject=subject_name=JenMale7;subject_sex=Male',
-                                         '##subject=subject_name=JenMale6;subject_sex=Male', '##sample=sample_name=JenMale6;subject_name=JenMale6', '##sample=sample_name=Wilds2-3;subject_name=Wilds2-3',
+                                         '##Assembly_name=GRCz10', '##subject=subject_name=Wilds2-3', '##subject=subject_name=Zon9',
+                                         '##subject=subject_name=JenMale7;subject_sex=Male', '##subject=subject_name=JenMale6;subject_sex=Male',
+                                         '##sample=sample_name=JenMale6;subject_name=JenMale6', '##sample=sample_name=Wilds2-3;subject_name=Wilds2-3',
                                          '##sample=sample_name=Zon9;subject_name=Zon9', '##sample=sample_name=JenMale7;subject_name=JenMale7']
+
 
 
         assert unique_alt_lines_to_add == ['##ALT=<ID=DEL,Description="Deletion">', '##ALT=<ID=DUP,Description="Duplication">']
